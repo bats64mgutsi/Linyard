b5f1b110d913fbc6581b42ff8664d13b0c9c4c58
Batandwa Mgutsi <B.mgutsi@gmail.com>
2019-12-12 22:43:04 +0200

    Initial commit with the initial folder structure to use

---
+++ engine/lib/linyard.dart
@@ -0,0 +1,6 @@
+/**
+ * Linyad is a web-multiplayer first game engine that aims to be simple but highly modable.
+ * The idea is to enable the building of complex scenes, objects and structures from smaller ones.
+ */
+
+library linyard;
---
+++ engine/lib/src/rendering.dart
@@ -0,0 +1,6 @@
+
+part of linyard;
+
+/**
+ * The vertex and fragment shader to be used 
+ */
