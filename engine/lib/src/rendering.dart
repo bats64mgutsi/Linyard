04f66718d421558ce71a1845aba78a9136208ea8
Batandwa Mgutsi <B.mgutsi@gmail.com>
2019-12-13 22:18:53 +0200

    Remove Renderer.pause and Renderer.resume as they will not be usable to any renderer since it will be running under the tick of the game world. Change Renderer.setClearColor to the setter clearColor. Implemented 1st version of GlesRenderer, webgl only for now

--- engine/lib/src/rendering.dart
+++ engine/lib/src/rendering.dart
@@ -52,7 +52,7 @@ class ContextShaderProgram extends ShaderProgram {
 class Renderable {
 
   final ShaderProgram shaderProgram;
-  final Float32List   vertices;
+  final Float32List   vertices;     // vec3
   final Int32List     indices;
   final Float32List   colors;
   final Matrix3       transform;
@@ -73,7 +73,7 @@ enum GraphicsLib{
 }
 
 
-/// A [Renderer] consumes [Renderables] and draws them to whatever surface it renders it.
+/// A [Renderer] consumes [Renderables] and draws them to whatever surface it renders to.
 abstract class Renderer{
 
   /// The graphics library used by this renderer
@@ -82,18 +82,12 @@ abstract class Renderer{
   /// Initialise resources and be ready for consuming renderables
   void initialise();
 
-  /// Pause rendering
-  void pause();
-
-  /// Resume rendering
-  void resume();
-
   /// Release all resources and be ready for destruction. It is assumed that after calling this method
   /// this renderer is no longer usable.
   void destroy();
 
   /// Set the background color of the drawing surface to [color]
-  void setClearColor(Vector4 color);
+  set clearColor(Vector4 color);
 
   /// Add a new [Renderable] to be drawn
   void add(Renderable renderable);
@@ -109,4 +103,126 @@ abstract class Renderer{
 
   /// Draw all the visible renderables
   void draw();
+}
+
+
+/// An error callback
+typedef void ErrorCallback(Object error);
+
+/// A Gles2 renderer. This renderer should work on web, mobile and desktop
+class GlesRenderer implements Renderer{
+
+  final RenderingContext gl;
+  final ErrorCallback onError;
+
+  GlesRenderer({this.gl, this.onError});
+
+  final List<Renderable> visible = List();
+  final List<Renderable> hidden  = List();
+
+  /// The graphics library used by this renderer, which is OpenGLEs
+  GraphicsLib get graphicsLib => GraphicsLib.GLES;
+
+  /// The background color of the rendering surface in ARGB format.
+  set clearColor(Vector4 color) => gl.clearColor(color.r, color.g, color.b, color.a);
+
+  /// Call this to let the renderer know the size of the viewport the image will be shown
+  void viewport(int width, int height) => gl.viewport(0, 0, width, height);
+
+  /// Initialise a rendering surface. [Renderable] objects should be added after 
+  /// this method is called.
+  @override
+  void initialise({int width, int height}) {
+
+    // Compile shaders and link the shaders
+    String vShader = """
+    attribute vec3 position;
+    attribute vec4 color;
+    attribute mat3 transform;
+
+    varying vec4 vColor;
+    void main(){
+      gl_Position = position*transform;
+      vColor = color;
+    }
+    """;
+
+    String fShader = """
+    varying vec4 vColor;
+
+    void main(){
+      gl_FragColor = vColor;
+    }
+    """;
+
+    Shader vs = gl.createShader(WebGL.VERTEX_SHADER);
+    gl.shaderSource(vs, vShader);
+    gl.compileShader(vs);
+
+    Shader fs = gl.createShader(WebGL.FRAGMENT_SHADER);
+    gl.shaderSource(fs, fShader);
+    gl.compileShader(fs);
+
+    Program program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    // Check if shaders were compiled properly
+    if (!gl.getShaderParameter(vs, WebGL.COMPILE_STATUS)) { 
+      onError(gl.getShaderInfoLog(vs));
+    }
+  
+    if (!gl.getShaderParameter(fs, WebGL.COMPILE_STATUS)) { 
+      onError(gl.getShaderInfoLog(fs));
+    }
+  
+    if (!gl.getProgramParameter(program, WebGL.LINK_STATUS)) { 
+      onError(gl.getProgramInfoLog(program));
+    }
+
+  }
+
+  /// Releases all rendering resources. After this method is called the renderer is no longer usable
+  @override
+  void destroy(){
+
+    //TODO: Implement
+  }
+
+  /// Adds [renderable] to the list of renderables. Initially the [Renderable] is visible.
+  /// To hide it call [hide]. The corresponding vbo objects will be created and filled with
+  /// [renderable] data. Therefore this method should only be called after [initialise] is
+  /// called.
+  @override
+  void add(Renderable renderable){
+
+    /// create vbos and add the object to list of visible renderables
+  }
+
+  /// Removed [renderable] as an object to be drawn. The corresponding vbos will be destroyed.
+  /// This [renderable] can be added back again.
+  @override
+  Renderable remove(Renderable renderable){
+
+    //TODO: implement
+  }
+
+  /// Pause the rendering of [renderable]
+  void hide(Renderable renderable){
+    if(visible.remove(renderable))
+      hidden.add(renderable);
+  }
+
+  /// Resume the rendering of [renderable]
+  void show(Renderable renderable){
+    if(hidden.remove(renderable))
+      visible.add(renderable);
+  }
+
+  /// Draw all the visible renderables
+  void draw(){
+    gl.clear(WebGL.COLOR_BUFFER_BIT);
+  }
 }
