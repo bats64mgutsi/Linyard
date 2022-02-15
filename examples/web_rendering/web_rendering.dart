6b341f0c092e58f726fd51d7f1e7c3b5f0d10660
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-02-15 13:36:47 +0200

    Makes triangle rotate

--- examples/web_rendering/web_rendering.dart
+++ examples/web_rendering/web_rendering.dart
@@ -11,24 +11,30 @@ import 'package:linyard/src/platform/webgl_server.dart';
 Renderer renderer;
 RenderingContext ctx;
 
-void draw(num delta){
-  int width = (window.innerWidth*.9).toInt();
-  int height = (window.innerHeight*.9).toInt();
+List<LoadedRenderable> loadedRenderables = [];
+
+void draw(num delta) {
+  int height = (window.innerHeight * .9).toInt();
+  int width = height;
   ctx.canvas.width = width;
   ctx.canvas.height = height;
   renderer.viewport(width, height);
+
+  loadedRenderables.forEach((renderable) {
+    renderable.transform.rotateZ(0.0000001 * delta);
+  });
+
   renderer.draw();
   window.animationFrame.then(draw);
 }
- 
-void main() {
 
+void main() {
   // Get the view where the image will be shown
   CanvasElement canvas = document.getElementById("view");
   var gl = ctx = canvas.getContext3d();
-  if(gl == null){
+  if (gl == null) {
     showError("Unfortunately your browser does not support WebGL");
-    return; 
+    return;
   }
 
   // Initialise a web renderer
@@ -37,36 +43,38 @@ void main() {
   renderer.clearColor = Vector4.random();
 
   // Add a triangle to be drawn
-  renderer.add(
-    Renderable(
-      vertices: Float32List.fromList([
-        -0.5, 0.5, 0.0,
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-      ],),
+  final r = renderer.add(Renderable(
+    vertices: Float32List.fromList(
+      [
+        -0.5,
+        0.5,
+        0.0,
+        -0.5,
+        -0.5,
+        0.0,
+        0.5,
+        -0.5,
+        0.0,
+      ],
+    ),
 
-      indices: Int32List.fromList([
-        1, 2, 3
-      ]),
+    indices: Int32List.fromList([1, 2, 3]),
 
-      colors: Float32List.fromList(
-        Colors.turquoise.storage+
-        Colors.red.storage+
-        Colors.green.storage
-      ),
+    colors: Float32List.fromList(
+        Colors.turquoise.storage + Colors.red.storage + Colors.green.storage),
 
-      //Add a bit of skewness
-      transform: Matrix4.rotationZ(0.13),
-    )
-  );
+    //Add a bit of skewness
+    transform: Matrix4.rotationZ(0.13),
+  ));
+
+  loadedRenderables.add(r);
 
   // Start drawing
   window.animationFrame.then(draw);
 }
 
-
 String errorMessages = "";
-void showError(Object error){
+void showError(Object error) {
   errorMessages += "$error<br />";
   document.getElementById("errorDiv").innerHtml = errorMessages;
 }
